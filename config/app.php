62aaa249bee79abd55682001af64856008434956
vagrant <vagrant@homestead>
2017-06-08 22:55:04 +0000

    added cors

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+
 
         /*
          * Application Service Providers...
